2c50a9564af54edd5768c6b210e73fbd03737e5d
di <zhangdi8501@gmail.com>
2018-04-12 21:10:46 -0400

    first commit

---
+++ src/com/climbstairs/di/Climbstairs.java
@@ -0,0 +1,19 @@
+package com.climbstairs.di;
+
+public class Climbstairs {
+	public int climbstairs (int n){
+		if (n==1){
+			return 1;
+		}
+		if(n==2){
+			return 2;
+		}
+		int current = 2; 
+		int previous = 1;
+		for(int i = 3; i <= n; i++){
+			current = current + previous;
+			previous = current - previous;
+		}
+		return current;
+	}
+}
